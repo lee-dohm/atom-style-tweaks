372dea1c92831aa321e43781907501404721feec
Lee Dohm <lee@lee-dohm.com>
2017-01-06 21:25:03 -0800

    :arrow_up: oauth2@0.8.2

--- mix.exs
+++ mix.exs
@@ -43,16 +43,17 @@ defmodule AtomStyleTweaks.Mixfile do
 
   defp deps do
     [
-      {:phoenix, "~> 1.2.1"},
-      {:phoenix_pubsub, "~> 1.0"},
-      {:phoenix_ecto, "~> 3.0"},
-      {:postgrex, ">= 0.0.0"},
-      {:phoenix_html, "~> 2.6"},
-      {:phoenix_live_reload, "~> 1.0", only: :dev},
-      {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
+      {:gettext, "~> 0.11"},
+      {:oauth2, "~> 0.8.2"},
+      {:phoenix_ecto, "~> 3.0"},
+      {:phoenix_html, "~> 2.6"},
+      {:phoenix_pubsub, "~> 1.0"},
+      {:phoenix, "~> 1.2.1"},
+      {:postgrex, ">= 0.0.0"},
+      {:dotenv, "~> 2.0.0", only: :dev},
       {:ex_doc, "~> 0.14.5", only: :dev},
-      {:dotenv, "~> 2.0.0", only: :dev}
+      {:phoenix_live_reload, "~> 1.0", only: :dev}
     ]
   end
 
