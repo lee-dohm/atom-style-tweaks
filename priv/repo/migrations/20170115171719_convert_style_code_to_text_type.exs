6adb0f58faaf455d72f6114b6cdef8eeadc43c99
Lee Dohm <lee-dohm@github.com>
2017-01-15 09:20:13 -0800

    Change styles.code type to text from varchar(255)

---
+++ priv/repo/migrations/20170115171719_convert_style_code_to_text_type.exs
@@ -0,0 +1,9 @@
+defmodule AtomStyleTweaks.Repo.Migrations.ConvertStyleCodeToTextType do
+  use Ecto.Migration
+
+  def change do
+    alter table(:styles) do
+      modify :code, :text
+    end
+  end
+end
