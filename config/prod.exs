e86f3f8803745734a94da1b24e8677624c594978
Lee Dohm <lee-dohm@github.com>
2017-01-14 18:18:33 -0800

    Replace Heroku temporary app name

--- config/prod.exs
+++ config/prod.exs
@@ -13,7 +13,7 @@ use Mix.Config
 # which you typically run after static files are built.
 config :atom_style_tweaks, AtomStyleTweaks.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "obscure-shore-84484.herokuapp.com", port: 80],
+  url: [host: "www.atom-tweaks.com", port: 80],
   cache_static_manifest: "priv/static/manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
