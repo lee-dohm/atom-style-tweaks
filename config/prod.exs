1315456c5f641a0b69708674e079c6370a9201db
Lee Dohm <lee-dohm@users.noreply.github.com>
2017-05-23 13:20:59 -0700

    Force SSL in production

--- config/prod.exs
+++ config/prod.exs
@@ -15,7 +15,8 @@ config :atom_style_tweaks, AtomStyleTweaks.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [host: "www.atom-tweaks.com", port: 80],
   cache_static_manifest: "priv/static/manifest.json",
-  secret_key_base: System.get_env("SECRET_KEY_BASE")
+  secret_key_base: System.get_env("SECRET_KEY_BASE"),
+  force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
 # Do not print debug messages in production
 config :logger, level: :info
