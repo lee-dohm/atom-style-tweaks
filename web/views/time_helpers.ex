8efc114ab86bbafbddd360d600f141b508effb77
Lee Dohm <lee-dohm@users.noreply.github.com>
2017-05-24 13:35:38 -0700

    :fire: Pattern for parsing durations

--- web/views/time_helpers.ex
+++ web/views/time_helpers.ex
@@ -1,8 +1,6 @@
 defmodule AtomStyleTweaks.TimeHelpers do
   use Phoenix.HTML
 
-  @duration_pattern ~r/P((?<years>\d+)Y)?((?<months>\d+)M)?((?<days>\d+)D)?(T((?<hours>\d+)H)?((?<minutes>\d+)M)?((?<seconds>\d+)S)?)?/
-
   def relative_time(from) do
     content_tag(:"time-ago", Timex.from_now(from, Gettext.get_locale(AtomStyleTweaks.Gettext)), title: from)
   end
