cfff59eb083f6d0ac32b18b6cd31e043aaaa13be
Lee Dohm <lee-dohm@users.noreply.github.com>
2017-05-24 14:58:27 -0700

    Don't use horizontal rule and italicize trademark notice

--- lib/atom_style_tweaks/markdown_engine.ex
+++ lib/atom_style_tweaks/markdown_engine.ex
@@ -2,6 +2,6 @@ defmodule AtomStyleTweaks.MarkdownEngine do
   @behaviour Slime.Parser.EmbeddedEngine
 
   def render(text, _options) do
-    Cmark.to_html(text)
+    Cmark.to_html(text, [:safe, :smart])
   end
 end
