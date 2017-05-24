470dfcdf78df8a8443673691b832f57f39e75f91
Lee Dohm <lee-dohm@users.noreply.github.com>
2017-05-24 12:40:39 -0700

    Add some user seeds

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -9,3 +9,9 @@
 #
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
+
+alias AtomStyleTweaks.Repo
+alias AtomStyleTweaks.User
+
+Repo.insert!(%User{}, %{name: "lee-dohm", github_id: 1038121, avatar_url: "https://avatars.githubusercontent.com/u/1038121?v=3", site_admin: true})
+Repo.insert!(%User{}, %{name: "hubot", github_id: 480938, avatar_url: "https://avatars3.githubusercontent.com/u/480938?v=3&s=400", site_admin: false})
