39cd0d4bd59537dbe554226cb6ca87a00939dcd3
Lee Dohm <lee-dohm@users.noreply.github.com>
2017-05-24 18:04:23 -0700

    Add some PageController tests

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -1,8 +1,50 @@
 defmodule AtomStyleTweaks.PageController.Test do
   use AtomStyleTweaks.ConnCase
 
+  import AtomStyleTweaks.Factory
+
   test "GET /", %{conn: conn} do
-    conn = get conn, "/"
+    conn = get(conn, page_path(conn, :index))
+
     assert html_response(conn, 200) =~ "Atom Tweaks"
+    assert html_response(conn, 200) =~ "href=\"#{page_path(conn, :index)}\""
+  end
+
+  test "index shows a list of tweaks" do
+    conn = build_conn()
+    styles = insert_list(3, :style)
+
+    conn = get(conn, page_path(conn, :index))
+
+    Enum.each(styles, fn(style) ->
+      assert html_response(conn, 200) =~ style.title
+      assert html_response(conn, 200) =~ style.user.name
+    end)
+  end
+
+  test "index shows styles tab", %{conn: conn} do
+    conn = get(conn, page_path(conn, :index))
+
+    assert html_response(conn, 200) =~ "Styles"
+  end
+
+  test "index shows About link", %{conn: conn} do
+    conn = get(conn, page_path(conn, :index))
+
+    assert html_response(conn, 200) =~ "About"
+    assert html_response(conn, 200) =~ page_path(conn, :about)
+  end
+
+  test "index shows the GitHub link" do
+    conn = build_conn()
+    conn = get(conn, page_path(conn, :index))
+
+    assert html_response(conn, 200) =~ "https://github.com/lee-dohm/atom-style-tweaks"
+  end
+
+  test "about page shows some about text", %{conn: conn} do
+    conn = get(conn, page_path(conn, :about))
+
+    assert html_response(conn, 200) =~ "About Atom Tweaks"
   end
 end
---
+++ test/support/factory.ex
@@ -0,0 +1,25 @@
+defmodule AtomStyleTweaks.Factory do
+  use ExMachina.Ecto, repo: AtomStyleTweaks.Repo
+
+  alias FakerElixir, as: Faker
+
+  alias AtomStyleTweaks.Style
+  alias AtomStyleTweaks.User
+
+  def user_factory do
+    %User{
+      name: Faker.Name.name(),
+      site_admin: false,
+      github_id: Faker.Helper.pick(1..100),
+      avatar_url: Faker.Avatar.robohash()
+    }
+  end
+
+  def style_factory do
+    %Style{
+      title: Faker.Lorem.words(2..4),
+      code: "atom-text-editor { font-style: normal; }",
+      user: build(:user)
+    }
+  end
+end
