9a879220b50840156cd7824e5fa4dd46c4056ba7
Lee Dohm <lee-dohm@users.noreply.github.com>
2017-05-24 18:43:22 -0700

    :art:

--- test/controllers/page_controller_test.exs
+++ test/controllers/page_controller_test.exs
@@ -1,13 +1,41 @@
 defmodule AtomStyleTweaks.PageController.Test do
   use AtomStyleTweaks.ConnCase
+  use Plug.Test
 
+  require Logger
   import AtomStyleTweaks.Factory
 
-  test "GET /", %{conn: conn} do
-    conn = get(conn, page_path(conn, :index))
+  def index do
+    conn = build_conn()
+
+    get(conn, page_path(conn, :index))
+  end
+
+  def index(:logged_in) do
+    conn = build_conn()
+    user = build(:user)
+    conn = init_test_session(conn, %{current_user: user})
+
+    get(conn, page_path(conn, :index))
+  end
+
+  test "index shows home page link" do
+    conn = index()
 
     assert html_response(conn, 200) =~ "Atom Tweaks"
     assert html_response(conn, 200) =~ "href=\"#{page_path(conn, :index)}\""
+  end
+
+  test "index does not show 'New tweak' button when not logged in" do
+    conn = index()
+
+    refute html_response(conn, 200) =~ "New tweak"
+  end
+
+  test "index shows new tweak button when logged in" do
+    conn = index(:logged_in)
+
+    assert html_response(conn, 200) =~ "New tweak"
   end
 
   test "index shows a list of tweaks" do
