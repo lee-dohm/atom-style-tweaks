f450e9fa25ef8c776799a5765f6743ac172ddbd6
Lee Dohm <lee-dohm@users.noreply.github.com>
2017-05-27 09:08:38 -0700

    Add pagination to home page

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -13,10 +13,7 @@ defmodule AtomStyleTweaks.PageController do
 
     render conn, :index,
       tweaks: page.entries,
-      page_number: page.page_number,
-      page_size: page.page_size,
-      total_pages: page.total_pages,
-      total_entries: page.total_entries,
+      page: page,
       params: params
   end
 
--- web/views/page_view.ex
+++ web/views/page_view.ex
@@ -2,4 +2,5 @@ defmodule AtomStyleTweaks.PageView do
   use AtomStyleTweaks.Web, :view
 
   alias AtomStyleTweaks.TweakView
+  use Scrivener.HTML
 end
