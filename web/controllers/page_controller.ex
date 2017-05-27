3983e81ecc424e6800b69158ae583c41dbba2dd7
Lee Dohm <lee-dohm@users.noreply.github.com>
2017-05-27 08:21:42 -0700

    Add pagination logic to home page

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -9,9 +9,15 @@ defmodule AtomStyleTweaks.PageController do
             |> Tweak.preload
 
     query = if params["type"], do: Tweak.by_type(query, params["type"]), else: query
-    tweaks = Repo.all(query)
+    page = Repo.paginate(query, params)
 
-    render(conn, "index.html", tweaks: tweaks, params: params)
+    render conn, :index,
+      tweaks: page.entries,
+      page_number: page.page_number,
+      page_size: page.page_size,
+      total_pages: page.total_pages,
+      total_entries: page.total_entries,
+      params: params
   end
 
   def about(conn, _params) do
