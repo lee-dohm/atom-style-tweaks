0878074e0289a7651c7f8b6eefb9ee353e5b2fea
Lee Dohm <lee-dohm@users.noreply.github.com>
2017-06-01 15:06:20 -0700

    :fire: Unused function

--- web/views/error_helpers.ex
+++ web/views/error_helpers.ex
@@ -6,15 +6,6 @@ defmodule AtomStyleTweaks.ErrorHelpers do
   use Phoenix.HTML
 
   @doc """
-  Generates tag for inlined form input errors.
-  """
-  def error_tag(form, field) do
-    if error = form.errors[field] do
-      content_tag :span, translate_error(error), class: "help-block"
-    end
-  end
-
-  @doc """
   Translates an error message using gettext.
   """
   def translate_error({msg, opts}) do
