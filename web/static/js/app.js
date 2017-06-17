fb670cdc180f92a7896ca4632f42e4b57b286c7d
Lee Dohm <lee-dohm@users.noreply.github.com>
2017-06-17 11:45:22 -0700

    Move all JS out of templates

--- web/static/js/app.js
+++ web/static/js/app.js
@@ -19,3 +19,6 @@ import 'phoenix_html'
 // paths "./socket" or full ones "web/static/js/socket".
 
 // import socket from "./socket"
+
+hljs.initHighlightingOnLoad();
+new Clipboard('#copy-button');
