5f866ca99acf3b88e09a19a6e0ae4eacf7057abe
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2017-09-22 10:38:58 -0700

    Update static manifest location

--- config/prod.exs
+++ config/prod.exs
@@ -14,7 +14,7 @@ use Mix.Config
 config :atom_style_tweaks, AtomStyleTweaks.Endpoint,
   http: [port: {:system, "PORT"}],
   url: [host: "www.atom-tweaks.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json",
+  cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE"),
   force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
