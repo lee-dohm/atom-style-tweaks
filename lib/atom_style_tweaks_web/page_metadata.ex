d6b7c9f3bd619281677ea54c84723fd393b83fea
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2017-10-06 20:44:51 -0700

    :memo:

--- lib/atom_style_tweaks_web/page_metadata.ex
+++ lib/atom_style_tweaks_web/page_metadata.ex
@@ -1,8 +1,15 @@
 defmodule AtomStyleTweaksWeb.PageMetadata do
+  @moduledoc """
+  A system for easily setting metadata for the page before it is rendered.
+  """
   use Phoenix.HTML
 
   import Plug.Conn, only: [assign: 3]
 
+  @doc """
+  Renders the metadata for the page, if it was set.
+  """
+  @spec render(Plug.Conn.t) :: Phoenix.HTML.safe | nil
   def render(conn) do
     case conn.assigns[:page_metadata] do
       nil -> nil
@@ -11,6 +18,10 @@ defmodule AtomStyleTweaksWeb.PageMetadata do
     end
   end
 
+  @doc """
+  Sets the metadata for the page.
+  """
+  @spec set(Plug.Conn.t, Map.t) :: Plug.Conn.t
   def set(conn, metadata) do
     metadata = Map.merge(%{
         "og:url": "#{conn.scheme}://#{conn.host}#{conn.request_path}",
