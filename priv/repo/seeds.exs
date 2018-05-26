e6a01ef52e05ff955e123aa823fba29e97774335
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2018-05-26 12:48:30 -0700

    Don't load seeds in the test environment

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -14,25 +14,27 @@ alias AtomTweaks.Repo
 alias AtomTweaksWeb.Tweak
 alias AtomTweaksWeb.User
 
-users = [
-  %{name: "lee-dohm", github_id: 1_038_121, avatar_url: "https://avatars.githubusercontent.com/u/1038121?v=3", site_admin: true},
-  %{name: "hubot", github_id: 480_938, avatar_url: "https://avatars3.githubusercontent.com/u/480938?v=3&s=400", site_admin: false}
-]
+if System.get_env("MIX_ENV") != "test" do
+  users = [
+    %{name: "lee-dohm", github_id: 1_038_121, avatar_url: "https://avatars.githubusercontent.com/u/1038121?v=3", site_admin: true},
+    %{name: "hubot", github_id: 480_938, avatar_url: "https://avatars3.githubusercontent.com/u/480938?v=3&s=400", site_admin: false}
+  ]
 
-Enum.each(users, fn(user) ->
-  unless Repo.get_by(User, github_id: user.github_id) do
-    Repo.insert!(User.changeset(%User{}, user))
-  end
-end)
+  Enum.each(users, fn(user) ->
+    unless Repo.get_by(User, github_id: user.github_id) do
+      Repo.insert!(User.changeset(%User{}, user))
+    end
+  end)
 
-tweaks = [
-  %{title: "Test tweak", code: "atom-text-editor { font-style: normal; }", type: "style"}
-]
+  tweaks = [
+    %{title: "Test tweak", code: "atom-text-editor { font-style: normal; }", type: "style"}
+  ]
 
-user = Repo.get_by!(User, name: "lee-dohm")
-Enum.each(tweaks, fn(tweak) ->
-  unless Repo.get_by(Tweak, title: tweak.title) do
-    tweak = Map.put_new(tweak, :created_by, user.id)
-    Repo.insert!(Tweak.changeset(%Tweak{}, tweak))
-  end
-end)
+  user = Repo.get_by!(User, name: "lee-dohm")
+  Enum.each(tweaks, fn(tweak) ->
+    unless Repo.get_by(Tweak, title: tweak.title) do
+      tweak = Map.put_new(tweak, :created_by, user.id)
+      Repo.insert!(Tweak.changeset(%Tweak{}, tweak))
+    end
+  end)
+end
