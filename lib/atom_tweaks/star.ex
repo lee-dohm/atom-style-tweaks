d02f78deeaa0df5f7fb834f92d3ad852bf688090
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2018-05-29 18:12:13 -0700

    :memo:

--- lib/atom_tweaks/star.ex
+++ lib/atom_tweaks/star.ex
@@ -1,6 +1,9 @@
 defmodule AtomTweaks.Star do
   @moduledoc """
   Represents that a user has marked a tweak as a favorite of theirs.
+
+  The list of tweaks that a user has starred is called their "stars". The list of users that have
+  starred a specific tweak is called the tweak's "stargazers".
   """
   use Ecto.Schema
 
