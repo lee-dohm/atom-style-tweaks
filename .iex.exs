b6e1307af4ad49f81209eab6bc011c98a868b9f4
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2018-06-02 18:37:26 -0700

    Add some more utilities to .iex.exs

--- .iex.exs
+++ .iex.exs
@@ -1,3 +1,13 @@
+import_file_if_available "~/.iex.exs"
+
+use Phoenix.HTML
+
+import Ecto.Query
+import Phoenix.HTML.Safe, only: [to_iodata: 1]
+
+alias AtomTweaks.Accounts
 alias AtomTweaks.Accounts.User
+alias AtomTweaks.Repo
+alias AtomTweaks.Tweaks
 alias AtomTweaks.Tweaks.Star
 alias AtomTweaks.Tweaks.Tweak
