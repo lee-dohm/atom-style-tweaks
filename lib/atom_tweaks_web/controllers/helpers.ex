4545c4a28ba3c01dd992161646e13bf7c9643032
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2018-06-03 17:00:56 -0700

    :memo:

--- lib/atom_tweaks_web/controllers/helpers.ex
+++ lib/atom_tweaks_web/controllers/helpers.ex
@@ -1,8 +1,11 @@
 defmodule AtomTweaksWeb.ControllerHelpers do
+  @moduledoc """
+  Helper functions for `Controllers`.
+  """
   alias Phoenix.Controller
 
   @doc """
-  Redirects back to the last user-navigated path.
+  Redirects back to the last user-navigated (in other words HTTP `GET`) path.
   """
   def redirect_back(conn, opts \\ []) do
     Controller.redirect(conn, to: NavigationHistory.last_path(conn, opts))
