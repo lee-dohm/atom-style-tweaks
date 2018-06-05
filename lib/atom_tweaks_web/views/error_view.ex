fbb7c1961788272670652b52c7deb469b6b6f6e7
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2018-06-05 13:03:16 -0700

    Add a handler for status 422

--- lib/atom_tweaks_web/views/error_view.ex
+++ lib/atom_tweaks_web/views/error_view.ex
@@ -1,13 +1,17 @@
 defmodule AtomTweaksWeb.ErrorView do
+  @doc """
+  View functions for HTTP error states.
+  """
   use AtomTweaksWeb, :view
 
-  def render("404.html", _assigns) do
-    "Page not found"
-  end
+  @doc """
+  Renders the page for the given template.
 
-  def render("500.html", _assigns) do
-    "Internal server error"
-  end
+  Since we don't have fancy error pages yet, we're just rendering the text of the status code.
+  """
+  def render("404.html", _assigns), do: "Page not found"
+  def render("422.html", _assigns), do: "Unprocessable entity"
+  def render("500.html", _assigns), do: "Internal server error"
 
   # In case no render clause matches or no
   # template is found, let's render it as 500
