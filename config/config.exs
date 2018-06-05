bae42332f2ead0b0e9628673fbf91b6cefcc6ffa
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2018-06-05 16:03:34 -0700

    Create the staging environment

--- config/config.exs
+++ config/config.exs
@@ -46,7 +46,7 @@ config :sentry,
   environment_name: Mix.env(),
   enable_source_code_context: true,
   root_source_code_path: File.cwd!(),
-  included_environments: [:prod]
+  included_environments: [:staging, :prod]
 
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
