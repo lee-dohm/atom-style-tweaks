1f053d392a6fd996beef4dd61b3ad2e9ea0b933b
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2018-06-07 11:40:49 -0700

    Pass only the information the view really needs

--- lib/atom_tweaks_web/controllers/page_controller.ex
+++ lib/atom_tweaks_web/controllers/page_controller.ex
@@ -1,20 +1,31 @@
 defmodule AtomTweaksWeb.PageController do
+  @moduledoc """
+  Handles requests for the root-level resources.
+  """
   use AtomTweaksWeb, :controller
 
   alias AtomTweaks.Tweaks.Tweak
 
+  @doc """
+  Renders the root-level index page.
+  """
   def index(conn, params) do
+    type = params["type"]
+
     query =
       Tweak
       |> Tweak.sorted()
       |> Tweak.preload()
 
-    query = if params["type"], do: Tweak.by_type(query, params["type"]), else: query
+    query = if type, do: Tweak.by_type(query, type), else: query
     tweaks = Repo.all(query)
 
-    render(conn, "index.html", tweaks: tweaks, params: params)
+    render(conn, "index.html", tweaks: tweaks, type: type)
   end
 
+  @doc """
+  Renders the about page.
+  """
   def about(conn, _params) do
     render(conn, "about.html")
   end
