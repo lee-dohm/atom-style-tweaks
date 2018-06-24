09853c2e09f0160401a3c285547ac8f0fe51ab54
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2018-06-24 13:39:06 -0700

    :memo:

--- test/support/assert_helpers.ex
+++ test/support/assert_helpers.ex
@@ -3,6 +3,10 @@ defmodule Support.AssertHelpers do
   Customized assertions for tests.
   """
 
+  @doc """
+  Determines if the `field` in `changeset` has an error.
+  """
+  @spec has_error_on?(Ecto.Changeset.t(), atom) :: boolean
   def has_error_on?(changeset, field) do
     Enum.any?(changeset.errors, fn error -> ^field = elem(error, 0) end)
   end
