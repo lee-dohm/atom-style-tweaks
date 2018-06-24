08cf429420e347643cbfc6913db0674ca727868f
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2018-06-23 22:06:18 -0700

    :memo:

--- lib/atom_tweaks/repo.ex
+++ lib/atom_tweaks/repo.ex
@@ -1,3 +1,18 @@
 defmodule AtomTweaks.Repo do
+  @moduledoc """
+  Defines the Atom Tweaks database repository.
+
+  ## Shared options
+
+  Almost all of the repository operations below accept the following options:
+
+  * `:timeout` - The time in milliseconds to wait for the query call to finish, `:infinity` will
+    wait indefinitely (default: 15000);
+  * `:pool_timeout` - The time in milliseconds to wait for calls to the pool to finish, `:infinity`
+    will wait indefinitely (default: 5000);
+  * `:log` - When false, does not log the query
+
+  Such cases will be explicitly documented as well as any extra options.
+  """
   use Ecto.Repo, otp_app: :atom_tweaks
 end
--- test/support/setup_helpers.ex
+++ test/support/setup_helpers.ex
@@ -2,8 +2,22 @@ defmodule Support.SetupHelpers do
   @moduledoc """
   Functions that are designed to be used as chained `setup` callbacks.
 
-  Each callback takes certain values from the `context` state and merges in new or updated values
-  upon return.
+  Each callback takes certain values from the `context` state and merges in new or updated values.
+  Because they only get supplied the `context` as a parameter, each function has a contract around
+  the keys that are used.
+
+  ## Categories
+
+  * `insert` functions create database records
+  * `request` functions navigate to web pages
+
+  ## Examples
+
+  Inserts a user, creates a tweak for that user, and then stars that tweak for that user:
+
+  ```
+  setup [:insert_user, :insert_tweak, :insert_star]
+  ```
   """
   import Phoenix.ConnTest
 
@@ -14,11 +28,58 @@ defmodule Support.SetupHelpers do
 
   @endpoint AtomTweaksWeb.Endpoint
 
+  @doc """
+  Inserts a site admin user into the database.
+
+  ## Outputs
+
+  * `context.user` - Site admin user record
+  """
+  def insert_site_admin(context)
+
+  def insert_site_admin(_context) do
+    user = insert(:user, site_admin: true)
+
+    {:ok, user: user}
+  end
+
+  @doc """
+  Stars a tweak by a user.
+
+  ## Inputs
+
+  * `context.user` - User to star the tweak
+  * `context.tweak` - Tweak to be starred
+
+  ## Outputs
+
+  * `context.star` - Star record
+  """
+  def insert_star(context)
+
   def insert_star(%{user: user, tweak: tweak}) do
     {:ok, star} = Accounts.star_tweak(user, tweak)
 
     {:ok, star: star}
   end
+
+  @doc """
+  Inserts a single tweak for a user.
+
+  ## Inputs
+
+  Inserts a tweak for (in order of priority):
+
+  1. `context.current_user`
+  1. `context.user`
+  1. Inserts a new user
+
+  ## Outputs
+
+  * `context.tweak` - New tweak record
+  * `context.user` - User record that created the tweak
+  """
+  def insert_tweak(context)
 
   def insert_tweak(%{current_user: user}) do
     tweak = insert(:tweak, user: user)
@@ -39,19 +100,30 @@ defmodule Support.SetupHelpers do
     {:ok, tweak: tweak, user: user}
   end
 
-  def invalid_request_user(_context) do
-    {:ok, request_user: build(:user)}
-  end
+  @doc """
+  Inserts a user into the database.
 
-  def request_user(_context) do
-    {:ok, request_user: insert(:user)}
-  end
+  ## Outputs
+
+  * `context.user` - New user record
+  """
+  def insert_user(context)
 
   def insert_user(_context) do
     user = insert(:user)
 
     {:ok, user: user}
   end
+
+  @doc """
+  Inserts a user with three tweaks into the database.
+
+  ## Outputs
+
+  * `context.user` - New user record
+  * `context.tweaks` - New tweak records
+  """
+  def insert_user_with_tweaks(context)
 
   def insert_user_with_tweaks(_context) do
     user = insert(:user)
@@ -60,11 +132,34 @@ defmodule Support.SetupHelpers do
     {:ok, user: user, tweaks: tweaks}
   end
 
-  def insert_site_admin(_context) do
-    user = insert(:user, site_admin: true)
+  @doc """
+  Creates an invalid set of parameters for a tweak.
 
-    {:ok, user: user}
+  ## Outputs
+
+  * `context.tweak_params` - Invalid params
+  """
+  def invalid_tweak_params(_context) do
+    {:ok, tweak_params: params_for(:tweak, title: "")}
   end
+
+  @doc """
+  Logs in as a user.
+
+  ## Inputs
+
+  * `context.conn` - `Plug.Conn` object
+
+  Logs in as (in priority order):
+
+  1. `context.user`
+  1. Inserts a new user into the database
+
+  ## Outputs
+
+  * `context.current_user` - User record used to log in
+  """
+  def log_in(context)
 
   def log_in(%{conn: conn, user: user}) do
     conn = Plug.Test.init_test_session(conn, %{current_user: user})
@@ -80,8 +175,32 @@ defmodule Support.SetupHelpers do
   end
 
   @doc """
-  Builds a create tweak request for a different user than the one logged in.
+  Creates a new user in the database to request a page.
+
+  ## Outputs
+
+  * `context.request_user` - User record that requests a page
   """
+  def request_user(_context) do
+    {:ok, request_user: insert(:user)}
+  end
+
+  @doc """
+  Requests the create tweak page.
+
+  ## Inputs
+
+  * `context.conn` - `Plug.Conn` object
+  * `context.current_user` - Currently logged in user record, if any
+  * `context.request_user` - User record requesting the page
+  * `context.tweak_params` - Parameters to use to create the tweak
+
+  ## Outputs
+
+  * `context.conn` - `Plug.Conn` object after the page is rendered
+  """
+  def request_create_tweak(context)
+
   def request_create_tweak(%{
         conn: conn,
         current_user: _,
@@ -95,9 +214,6 @@ defmodule Support.SetupHelpers do
     {:ok, conn: conn}
   end
 
-  @doc """
-  Builds a create tweak request for the logged in user.
-  """
   def request_create_tweak(%{conn: conn, current_user: user, tweak_params: tweak_params}) do
     params = %{"name" => user.name, "tweak" => tweak_params}
 
@@ -106,9 +222,6 @@ defmodule Support.SetupHelpers do
     {:ok, conn: conn}
   end
 
-  @doc """
-  Builds a create tweak request for the given user when nobody is logged in.
-  """
   def request_create_tweak(%{conn: conn, tweak_params: tweak_params, user: user}) do
     params = %{"name" => user.name, "tweak" => tweak_params}
 
@@ -118,8 +231,19 @@ defmodule Support.SetupHelpers do
   end
 
   @doc """
-  Builds an edit tweak request.
+  Requests the edit tweak page.
+
+  ## Inputs
+
+  * `context.conn` - `Plug.Conn` object
+  * `context.tweak` - Tweak record to edit
+
+  ## Outputs
+
+  * `context.conn` - `Plug.Conn` object after the page is rendered
   """
+  def request_edit_tweak(context)
+
   def request_edit_tweak(%{conn: conn, tweak: tweak}) do
     conn = get(conn, tweak_path(conn, :edit, tweak))
 
@@ -127,8 +251,18 @@ defmodule Support.SetupHelpers do
   end
 
   @doc """
-  Builds a new tweak request.
+  Requests the new tweak page.
+
+  ## Inputs
+
+  * `context.conn` - `Plug.Conn` object
+
+  ## Outputs
+
+  * `context.conn` - `Plug.Conn` object after the page is rendered
   """
+  def request_new_tweak(context)
+
   def request_new_tweak(%{conn: conn}) do
     conn = get(conn, tweak_path(conn, :new))
 
@@ -136,13 +270,41 @@ defmodule Support.SetupHelpers do
   end
 
   @doc """
-  Builds a show tweak request.
+  Requests the show tweak page.
+
+  ## Inputs
+
+  * `context.conn` - `Plug.Conn` object
+  * `context.tweak` - Tweak record to show
+
+  ## Outputs
+
+  * `context.conn` - `Plug.Conn` object after the page is rendered
   """
+  def request_show_tweak(context)
+
   def request_show_tweak(%{conn: conn, tweak: tweak}) do
     conn = get(conn, tweak_path(conn, :show, tweak))
 
     {:ok, conn: conn}
   end
+
+  @doc """
+  Requests the show user page.
+
+  ## Inputs
+
+  * `context.conn` - `Plug.Conn` object
+  * `context.current_user` - Currently logged in user record
+  * `context.request_user` - User record to show
+  * `context.user` - User record to show
+
+  ## Outputs
+
+  * `context.conn` - `Plug.Conn` object after the page is rendered
+  * `context.path` - Path that was navigated to
+  """
+  def request_show_user(context)
 
   def request_show_user(%{conn: conn, current_user: _, request_user: user}) do
     path = user_path(conn, :show, user.name)
@@ -159,13 +321,20 @@ defmodule Support.SetupHelpers do
   end
 
   @doc """
-  Navigates to the stars page for `user`.
+  Navigates to the stars page for a user.
 
-  ## Context modifications
+  ## Inputs
 
-  * **Update:** `conn` context variable with the current state after the navigation
-  * **Add:** `path` context variable with the path navigated to
+  * `context.conn` - `Plug.Conn` object
+  * `context.user` - User whose stars we will view
+
+  ## Outputs
+
+  * `context.conn` - Updated `Plug.Conn` object
+  * `context.path` - Path that was navigated to
   """
+  def request_stars(context)
+
   def request_stars(%{conn: conn, user: user}) do
     path = user_star_path(conn, :index, user)
     conn = get(conn, path)
@@ -175,9 +344,5 @@ defmodule Support.SetupHelpers do
 
   def valid_tweak_params(_context) do
     {:ok, tweak_params: params_for(:tweak)}
-  end
-
-  def invalid_tweak_params(_context) do
-    {:ok, tweak_params: params_for(:tweak, title: "")}
   end
 end
