d5852d5259e373ad946790854d389dd8ad841903
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2019-05-30 09:21:14 -0700

    :arrow_up: phoenix_octicons~>0.4

--- lib/atom_tweaks_web/endpoint.ex
+++ lib/atom_tweaks_web/endpoint.ex
@@ -31,7 +31,7 @@ defmodule AtomTweaksWeb.Endpoint do
     Plug.Parsers,
     parsers: [:urlencoded, :multipart, :json],
     pass: ["*/*"],
-    json_decoder: Poison
+    json_decoder: Jason
   )
 
   plug(Plug.MethodOverride)
