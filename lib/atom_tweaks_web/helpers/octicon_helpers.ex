a893e4bfbc13206698fc4dcd0c1dd687aea05a66
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2019-06-06 11:00:46 -0700

    Streamline the octicons styles

--- lib/atom_tweaks_web/helpers/octicon_helpers.ex
+++ lib/atom_tweaks_web/helpers/octicon_helpers.ex
@@ -9,22 +9,32 @@ defmodule AtomTweaksWeb.OcticonHelpers do
 
   @doc """
   Draws the appropriate Octicon for the given `tweak`.
+
+  ## Options
+
+  All options are passed through to `PhoenixOcticons.octicon/2` and are then passed to
+  `Octicons.to_svg/2`.
   """
-  @spec octicon_for(Tweak.t()) :: Phoenix.HTML.safe()
-  def octicon_for(tweak = %Tweak{}) do
+  @spec octicon_for(Tweak.t(), keyword) :: Phoenix.HTML.safe()
+  def octicon_for(tweak = %Tweak{}, options \\ []) do
     tweak
     |> icon_for_tweak
-    |> PhoenixOcticons.octicon()
+    |> PhoenixOcticons.octicon(options)
   end
 
   @doc """
   Draws the appropriate upsized Octicon for the given `tweak`.
+
+  ## Options
+
+  All options are passed through to `PhoenixOcticons.octicon/2` and are then passed to
+  `Octicons.to_svg/2`.
   """
-  @spec mega_octicon_for(Tweak.t()) :: Phoenix.HTML.safe()
-  def mega_octicon_for(tweak = %Tweak{}) do
+  @spec mega_octicon_for(Tweak.t(), keyword) :: Phoenix.HTML.safe()
+  def mega_octicon_for(tweak = %Tweak{}, options \\ []) do
     tweak
     |> icon_for_tweak
-    |> PhoenixOcticons.mega_octicon()
+    |> PhoenixOcticons.mega_octicon(options)
   end
 
   defp icon_for_tweak(%{type: "init"}), do: :code
