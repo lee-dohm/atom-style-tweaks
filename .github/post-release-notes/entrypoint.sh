2618b611fa0fa68329c26186c6b97664b9fdfe51
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2019-06-21 14:26:53 -0700

    Use --raw-output when asking jq to extract strings

--- .github/post-release-notes/entrypoint.sh
+++ .github/post-release-notes/entrypoint.sh
@@ -21,8 +21,8 @@ fi
 
 ./validate-release-notes "$file_path"
 
-title=$(jq .pull_request.title "$GITHUB_EVENT_PATH")
-detail_url=$(jq .pull_request.url "$GITHUB_EVENT_PATH")
+title=$(jq --raw-output .pull_request.title "$GITHUB_EVENT_PATH")
+detail_url=$(jq --raw-output .pull_request.url "$GITHUB_EVENT_PATH")
 
 httpie "authorization:token $ATOM_TWEAKS_API_KEY" "title=$title" "detail_url=$detail_url" \
        "description=@$file_path"
