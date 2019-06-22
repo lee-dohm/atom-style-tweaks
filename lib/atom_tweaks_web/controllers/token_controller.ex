0ffe668ab12c7e4e588bf06e846caf575922fe44
Lee Dohm <1038121+lee-dohm@users.noreply.github.com>
2019-06-22 13:07:47 -0700

    Add flow to create a new token

--- lib/atom_tweaks_web/controllers/token_controller.ex
+++ lib/atom_tweaks_web/controllers/token_controller.ex
@@ -6,10 +6,40 @@ defmodule AtomTweaksWeb.TokenController do
   use AtomTweaksWeb, :controller
 
   alias AtomTweaks.Accounts
+  alias AtomTweaks.Accounts.Token
   alias AtomTweaksWeb.ForbiddenUserError
+
+  require Logger
 
   plug(:ensure_authenticated_user)
   plug(:ensure_site_admin)
+
+  def create(conn, %{"token" => token_params, "user_id" => name}) do
+    user = Accounts.get_user!(name)
+
+    params =
+      token_params
+      |> Map.put("scopes", ["release_notes/write"])
+      |> Map.put("user_id", user.id)
+
+    changeset = Token.changeset(%Token{}, params)
+
+    case Repo.insert(changeset) do
+      {:ok, token} ->
+        redirect(conn, to: Routes.user_token_path(conn, :index, user))
+
+      {:error, changeset} ->
+        star_count = Accounts.count_stars(user)
+        tweak_count = Accounts.count_tweaks(user)
+
+        render(conn, "new.html",
+          changeset: changeset,
+          star_count: star_count,
+          tweak_count: tweak_count,
+          user: user
+        )
+    end
+  end
 
   def index(conn, %{"user_id" => name}) do
     user = Accounts.get_user!(name)
@@ -22,13 +52,30 @@ defmodule AtomTweaksWeb.TokenController do
     star_count = Accounts.count_stars(user)
     tweak_count = Accounts.count_tweaks(user)
 
-    render(
-      conn,
-      "index.html",
+    render(conn, "index.html",
       user: user,
       tokens: tokens,
       star_count: star_count,
       tweak_count: tweak_count
+    )
+  end
+
+  def new(conn, %{"user_id" => name}) do
+    user = Accounts.get_user!(name)
+
+    unless conn.assigns[:current_user].id == user.id do
+      raise(ForbiddenUserError, conn: conn)
+    end
+
+    changeset = Accounts.change_token(%Token{user: user})
+    star_count = Accounts.count_stars(user)
+    tweak_count = Accounts.count_tweaks(user)
+
+    render(conn, "new.html",
+      changeset: changeset,
+      star_count: star_count,
+      tweak_count: tweak_count,
+      user: user
     )
   end
 end
